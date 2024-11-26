c22d9177f5359c433884042242a38c05e6ec2619
LHRC19022008 <00001104192822sp@al.educacao.sp.gov.br>
2024-11-26 08:54:44 -0300

    Cabo

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Vida',
+    'Por que o amor é igual a Wi-Fi?',
+    'Porque é só o vizinho mudar a senha que você fica sem!'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Família',
+    'Por que ninguém discute com a sogra?',
+    'Porque ela já nasceu com o certificado de "Usuário Expert" em brigas.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Saúde',
+    'Por que o médico é igual ao ex?',
+    'Só aparece quando você tá mal e ainda te deixa pior com a conta.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Dinheiro',
+    'Qual a diferença entre você e o banco?',
+   'O banco te tira tudo e ainda te chama de cliente preferencial.'
 )
